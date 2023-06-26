eb12afeb03586562be4691b5602a9eaec5cb26bc
JoeHo888 <joe.hochotai@gmail.com>
2023-06-27 07:26:36 +0800

    Create Tag pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -21,6 +21,7 @@ exports.createPages = async ({ graphql, actions }) => {
               id
               frontmatter {
                 slug
+                tags
               }
             }
           }
@@ -29,6 +30,7 @@ exports.createPages = async ({ graphql, actions }) => {
               id
               frontmatter {
                 slug
+                tags
               }
             }
           }
@@ -39,7 +41,10 @@ exports.createPages = async ({ graphql, actions }) => {
     const projectsList = result.data.projects.nodes;
     const postsList = result.data.blogPosts.nodes;
 
+    const tagsList = [...new Set(postsList.map(x => x.frontmatter.tags).flat(1))].filter(n => n)
+
     const articleTemplate = require.resolve('./src/templates/article.jsx');
+    const TagTemplate = require.resolve('./src/templates/tag.jsx');
 
     postsList.forEach(post => {
         // The uid you assigned in Prismic is the slug!
@@ -66,4 +71,17 @@ exports.createPages = async ({ graphql, actions }) => {
           },
       })
   })
+
+  tagsList.forEach(tag => {
+    // The uid you assigned in Prismic is the slug!
+    createPage({
+        type: 'Tag',
+        path: `/tags/${tag.toLowerCase().split(" ").join("-")}`,
+        component: TagTemplate,
+        context: {
+            // Pass the unique ID (uid) through context so the template can filter by it
+            tag: tag,
+        },
+    })
+  })  
 }
---
+++ src/templates/tag.jsx
@@ -0,0 +1,128 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import { graphql } from "gatsby";
+import styled from "@emotion/styled";
+import Layout from "components/Layout";
+import Card from "components/Card";
+
+const TagTitle = styled("h1")`
+    margin-bottom: 1em;
+`
+
+
+const Tag = ({ posts, meta, tag }) => (
+    <>
+        <Helmet
+            title={meta.title}
+            meta={[
+                {
+                    name: `description`,
+                    content: meta.description,
+                },
+                {
+                    property: `og:title`,
+                    content: meta.title,
+                },
+                {
+                    property: `og:image`,
+                    content: meta.image,
+                },
+                {
+                    property: `og:description`,
+                    content: meta.description,
+                },
+                {
+                    property: `og:type`,
+                    content: `website`,
+                },
+                {
+                    name: `twitter:card`,
+                    content: `summary_large_image`,
+                },
+                {
+                    name: `twitter:title`,
+                    content: meta.title,
+                },
+                {
+                    name: `twitter:description`,
+                    content: meta.description,
+                },
+                {
+                    name: `twitter:creator`,
+                    content: meta.twitterAccount
+                },
+                {
+                    name: `twitter:image`,
+                    content: meta.image
+                },
+            ].concat(meta)}
+        />
+        <Layout>
+            <TagTitle>
+                Tag: {tag}
+            </TagTitle>
+
+            <div className="container">
+                <section className="post-feed">
+                    {posts.map((post, i) => (
+                        <Card
+                            key={i}
+                            date={post.frontmatter.date}
+                            slug={post.frontmatter.slug}
+                            title={post.frontmatter.title}
+                            description={post.frontmatter.description}
+                            category={post.frontmatter.category}
+                            imageLink={post.frontmatter.featuredImage.childImageSharp.fluid.src}
+                            tags={post.frontmatter.tags}
+                        />
+                    ))}
+                </section>
+            </div>
+
+        </Layout>
+    </>
+);
+
+export default ({ pageContext, data }) => {
+    const posts = data.blogPosts.nodes;
+    const tag = pageContext["tag"]
+    const meta = {
+        "title": "Blog Posts | Joe Ho Blog",
+        "description": "Blog Posts on various topics",
+        "author": "Joe Ho",
+        "twitterAccount": "@JoeHo31326982"
+    };
+    return (
+        <Tag posts={posts} meta={meta} tag={tag} />
+    )
+}
+
+Tag.propTypes = {
+    posts: PropTypes.array.isRequired,
+    meta: PropTypes.object.isRequired,
+    tag: PropTypes.string.isRequired,
+};
+
+export const query = graphql`
+    query TagQuery($tag: String) {
+        blogPosts: allMarkdownRemark(filter: {frontmatter: {tags: {eq: $tag}}}) {
+            nodes {
+                frontmatter {
+                    date(formatString: "MMMM DD, YYYY")
+                    slug
+                    title
+                    description
+                    tags
+                    featuredImage {
+                        childImageSharp {
+                            fluid(maxWidth: 800) {
+                                ...GatsbyImageSharpFluid
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
