7b18ba5fb93aea06ef0bc398d0fc8701d620d4ee
Joe Ho <joe.hochotai@gmail.com>
2023-09-15 17:24:23 +0800

    Centralize Newsletter

--- src/templates/Newsletter.jsx
+++ src/templates/Newsletter.jsx
@@ -602,90 +602,90 @@ const NewsletterContainer = styled("div")`
 `
 
 const NewsletterBody = () => {
-    return(
+    return (
         <div>
-        <form
-            action="https://app.convertkit.com/forms/5604785/subscriptions"
-            className="seva-form formkit-form"
-            method="post"
-            data-sv-form={5604785}
-            data-uid="f6342e534f"
-            data-format="inline"
-            data-version={5}
-            data-options='{"settings":{"after_subscribe":{"action":"message","success_message":"Success! Now check your email to confirm your subscription.","redirect_url":""},"analytics":{"google":null,"fathom":null,"facebook":null,"segment":null,"pinterest":null,"sparkloop":null,"googletagmanager":null},"modal":{"trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15},"powered_by":{"show":true,"url":"https://convertkit.com/features/forms?utm_campaign=poweredby&utm_content=form&utm_medium=referral&utm_source=dynamic"},"recaptcha":{"enabled":false},"return_visitor":{"action":"show","custom_content":""},"slide_in":{"display_in":"bottom_right","trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15},"sticky_bar":{"display_in":"top","trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15}},"version":"5"}'
-            min-width="400 500 600 700 800"
-            style={{ backgroundColor: "rgb(249, 250, 251)", borderRadius: 4 }}
-        >
-            <div className="formkit-background" style={{ opacity: "0.2" }} />
-            <div data-style="minimal">
-                <div
-                    className="formkit-header"
-                    data-element="header"
-                    style={{ color: "rgb(77, 77, 77)", fontSize: 27, fontWeight: 700 }}
-                >
-                    <h2>Join the Newsletter</h2>
-                </div>
-                <div
-                    className="formkit-subheader"
-                    data-element="subheader"
-                    style={{ color: "rgb(104, 104, 104)", fontSize: 18 }}
-                >
-                    Subscribe to get our latest content by email.
-                </div>
-                <ul
-                    className="formkit-alert formkit-alert-error"
-                    data-element="errors"
-                    data-group="alert"
-                />
-                <div
-                    data-element="fields"
-                    data-stacked="false"
-                    className="seva-fields formkit-fields"
-                >
-                    <div className="formkit-field">
-                        <input
-                            className="formkit-input"
-                            name="email_address"
-                            aria-label="Email Address"
-                            placeholder="Email Address"
-                            required=""
-                            type="email"
+            <form
+                action="https://app.convertkit.com/forms/5604785/subscriptions"
+                className="seva-form formkit-form"
+                method="post"
+                data-sv-form={5604785}
+                data-uid="f6342e534f"
+                data-format="inline"
+                data-version={5}
+                data-options='{"settings":{"after_subscribe":{"action":"message","success_message":"Success! Now check your email to confirm your subscription.","redirect_url":""},"analytics":{"google":null,"fathom":null,"facebook":null,"segment":null,"pinterest":null,"sparkloop":null,"googletagmanager":null},"modal":{"trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15},"powered_by":{"show":true,"url":"https://convertkit.com/features/forms?utm_campaign=poweredby&utm_content=form&utm_medium=referral&utm_source=dynamic"},"recaptcha":{"enabled":false},"return_visitor":{"action":"show","custom_content":""},"slide_in":{"display_in":"bottom_right","trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15},"sticky_bar":{"display_in":"top","trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15}},"version":"5"}'
+                min-width="400 500 600 700 800"
+                style={{ backgroundColor: "rgb(249, 250, 251)", borderRadius: 4, margin: "0 auto" }}
+            >
+                <div className="formkit-background" style={{ opacity: "0.2" }} />
+                <div data-style="minimal">
+                    <div
+                        className="formkit-header"
+                        data-element="header"
+                        style={{ color: "rgb(77, 77, 77)", fontSize: 27, fontWeight: 700 }}
+                    >
+                        <h2>Join the Newsletter</h2>
+                    </div>
+                    <div
+                        className="formkit-subheader"
+                        data-element="subheader"
+                        style={{ color: "rgb(104, 104, 104)", fontSize: 18 }}
+                    >
+                        Subscribe to get our latest content by email.
+                    </div>
+                    <ul
+                        className="formkit-alert formkit-alert-error"
+                        data-element="errors"
+                        data-group="alert"
+                    />
+                    <div
+                        data-element="fields"
+                        data-stacked="false"
+                        className="seva-fields formkit-fields"
+                    >
+                        <div className="formkit-field">
+                            <input
+                                className="formkit-input"
+                                name="email_address"
+                                aria-label="Email Address"
+                                placeholder="Email Address"
+                                required=""
+                                type="email"
+                                style={{
+                                    color: "rgb(0, 0, 0)",
+                                    borderColor: "rgb(227, 227, 227)",
+                                    borderRadius: 4,
+                                    fontWeight: 400
+                                }}
+                            />
+                        </div>
+                        <button
+                            data-element="submit"
+                            className="formkit-submit formkit-submit"
                             style={{
-                                color: "rgb(0, 0, 0)",
-                                borderColor: "rgb(227, 227, 227)",
+                                color: "rgb(255, 255, 255)",
+                                backgroundColor: "rgb(22, 119, 190)",
                                 borderRadius: 4,
                                 fontWeight: 400
                             }}
-                        />
+                        >
+                            <div className="formkit-spinner">
+                                <div />
+                                <div />
+                                <div />
+                            </div>
+                            <span className="">Subscribe</span>
+                        </button>
                     </div>
-                    <button
-                        data-element="submit"
-                        className="formkit-submit formkit-submit"
-                        style={{
-                            color: "rgb(255, 255, 255)",
-                            backgroundColor: "rgb(22, 119, 190)",
-                            borderRadius: 4,
-                            fontWeight: 400
-                        }}
+                    <div
+                        className="formkit-guarantee"
+                        data-element="guarantee"
+                        style={{ color: "rgb(77, 77, 77)", fontSize: 13, fontWeight: 400 }}
                     >
-                        <div className="formkit-spinner">
-                            <div />
-                            <div />
-                            <div />
-                        </div>
-                        <span className="">Subscribe</span>
-                    </button>
+                        We won't send you spam. Unsubscribe at any time.
+                    </div>
                 </div>
-                <div
-                    className="formkit-guarantee"
-                    data-element="guarantee"
-                    style={{ color: "rgb(77, 77, 77)", fontSize: 13, fontWeight: 400 }}
-                >
-                    We won't send you spam. Unsubscribe at any time.
-                </div>
-            </div>
-        </form>
-    </div>
+            </form>
+        </div>
     )
 }
 
